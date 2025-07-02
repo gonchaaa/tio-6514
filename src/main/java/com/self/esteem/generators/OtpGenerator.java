cf79eb0de2f723e26170ad070c6299de36d1729d
Qoncha A. Abbasova <QAAbbasova@bankofbaku.com>
2025-07-02 15:28:40 +0400

    otp added

--- src/main/java/com/self/esteem/generators/OtpGenerator.java
+++ src/main/java/com/self/esteem/generators/OtpGenerator.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class OtpGenerator {
     public String generateOtp() {
         Random random = new Random();
-        int otp = 1000 + random.nextInt(9000);
+        int otp = 100000 + random.nextInt(900000);
         return String.valueOf(otp);
     }
 }
